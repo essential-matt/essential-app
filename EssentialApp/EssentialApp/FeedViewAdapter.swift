92f79471f60ef20f4355c0f1922d1a7e5292c52f
Matteo Dallaturca <matteo.dallaturca@gmail.com>
2023-03-29 22:09:49 +0200

    Load more items on will display LoadMoreCell

--- EssentialApp/EssentialApp/FeedViewAdapter.swift
+++ EssentialApp/EssentialApp/FeedViewAdapter.swift
@@ -17,7 +17,7 @@ final class FeedViewAdapter: ResourceView {
     }
     
     func display(_ viewModel: Paginated<FeedImage>) {
-            controller?.display(viewModel.items.map { model in
+        let feed: [CellController] = viewModel.items.map { model in
             let adapter = ImageDataPresentationAdapter(loader: { [imageLoader] in
                 imageLoader(model.url)
             })
@@ -36,7 +36,14 @@ final class FeedViewAdapter: ResourceView {
                 mapper: UIImage.tryMake)
             
             return CellController(id: model, view)
-        })
+        }
+
+        let loadMore = LoadMoreCellController {
+            viewModel.loadMore?({ _ in })
+        }
+        let loadMoreSection = [CellController(id: UUID(), loadMore)]
+
+        controller?.display(feed, loadMoreSection)
     }
 }
 
